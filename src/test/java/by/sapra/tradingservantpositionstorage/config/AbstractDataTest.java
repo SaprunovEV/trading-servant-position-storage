e1d73403401ccf41df6cf9ede6d6d88c7680217c
Евгений Сапрунов <sapra10062018@gmail.com>
2026-07-29 11:31:22 +0300

    В AbstractDataTest были добавлены зависимости от liquibase

--- src/test/java/by/sapra/tradingservantpositionstorage/config/AbstractDataTest.java
+++ src/test/java/by/sapra/tradingservantpositionstorage/config/AbstractDataTest.java
@@ -38,7 +38,9 @@ public class AbstractDataTest {
         registry.add("spring.datasource.password", container::getPassword);
         registry.add("spring.datasource.url", container::getJdbcUrl);
         registry.add("spring.jpa.generate-ddl", () -> true);
-        registry.add("spring.datasource.driver-class-name", () -> container.getDatabaseName());
+        registry.add("spring.datasource.driver-class-name", () -> container.getDriverClassName());
+        registry.add("spring.liquibase.change-log", () -> "classpath:db/changelog/db.changelog-master.yaml");
+        registry.add("spring.liquibase.enabled", () -> true);
     }
 
     @AfterEach
