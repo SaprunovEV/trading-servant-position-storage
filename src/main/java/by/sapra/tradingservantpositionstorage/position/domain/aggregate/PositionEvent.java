9486bca5e3f42507e9d8782c3893785299ac7bdd
Евгений Сапрунов <sapra10062018@gmail.com>
2026-07-29 11:56:37 +0300

    Добавили аннотации для доступа к полям сущности (для JPA надо). Ну и конструкторы.

--- src/main/java/by/sapra/tradingservantpositionstorage/position/domain/aggregate/PositionEvent.java
+++ src/main/java/by/sapra/tradingservantpositionstorage/position/domain/aggregate/PositionEvent.java
@@ -2,8 +2,18 @@ package by.sapra.tradingservantpositionstorage.position.domain.aggregate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "position_event")
 public class PositionEvent {
     @Id
     private Long id;
--- src/main/java/by/sapra/tradingservantpositionstorage/position/infrostructure/outbox/model/Outbox.java
+++ src/main/java/by/sapra/tradingservantpositionstorage/position/infrostructure/outbox/model/Outbox.java
@@ -2,8 +2,18 @@ package by.sapra.tradingservantpositionstorage.position.infrostructure.outbox.mo
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "outbox")
 public class Outbox {
     @Id
     private Long id;
