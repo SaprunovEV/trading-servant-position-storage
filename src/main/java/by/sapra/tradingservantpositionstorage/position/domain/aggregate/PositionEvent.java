5139ba5ca13ab1a497cbd1829ce00b81fe06e769
Евгений Сапрунов <sapra10062018@gmail.com>
2026-07-29 11:31:44 +0300

    Они должны быть entity для того чтобы не падал тест

--- src/main/java/by/sapra/tradingservantpositionstorage/position/domain/aggregate/PositionEvent.java
+++ src/main/java/by/sapra/tradingservantpositionstorage/position/domain/aggregate/PositionEvent.java
@@ -1,6 +1,11 @@
 package by.sapra.tradingservantpositionstorage.position.domain.aggregate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class PositionEvent {
+    @Id
     private Long id;
     private String positionId;
 }
--- src/main/java/by/sapra/tradingservantpositionstorage/position/infrostructure/outbox/model/Outbox.java
+++ src/main/java/by/sapra/tradingservantpositionstorage/position/infrostructure/outbox/model/Outbox.java
@@ -1,6 +1,11 @@
 package by.sapra.tradingservantpositionstorage.position.infrostructure.outbox.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Outbox {
+    @Id
     private Long id;
     private String positionId;
 }
